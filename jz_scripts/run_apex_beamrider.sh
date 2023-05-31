e595da9a3b32cf1e2811a9291c10e138b18b99cd
Eleni Nisioti <elennisioti@gmail.com>
2023-05-31 10:43:29 +0200

    fix

--- jz_scripts/run_apex_beamrider.sh
+++ jz_scripts/run_apex_beamrider.sh
@@ -2,7 +2,6 @@
 #SBATCH -J beamrdiderapex
 #SBATCH -t 24:00:00
 #SBATCH --cpus-per-task 64
-#SBATCH --ntasks-per-node=10
 #SBATCH --output=/scratch/enisioti/cleanrl_log/jz_logs/%j.out
 #SBATCH --error=/scratch/enisioti/cleanrl_log/jz_logs/%j.err
 
